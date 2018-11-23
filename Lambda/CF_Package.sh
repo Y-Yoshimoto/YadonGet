9eb67f3fdeb790f5ed8ef9a1b3eb15db457bbe12
Y-Yoshimoto <mewyoshimoto@gmail.com>
2018-11-23 16:06:22 +0900

    仮作成成功

--- Lambda/CF_Package.sh
+++ Lambda/CF_Package.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+zip yadon.zip lambda_function.py -x "*.DS_Store"
 aws cloudformation package \
    --template-file YadonGet.yaml \
    --output-template-file serverless-output.yaml \
--- Lambda/CF_deploy.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-aws cloudformation deploy --template-file /Users/y-yoshimoto/Dropbox/APIGateway/Lambda/serverless-output.yaml --stack-name yadonget
