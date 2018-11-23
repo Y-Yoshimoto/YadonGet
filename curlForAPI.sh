517d9b89600224e7eeb543f5dbea338fcae428a7
Y-Yoshimoto <mewyoshimoto@gmail.com>
2018-11-23 17:11:08 +0900

    一連のデプロイ完了

--- curlForAPI.sh
+++ curlForAPI.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # curl -X POST -H "Content-Type: application/json" -d '{"number":20}' https://liuncvp3x8.execute-api.ap-northeast-1.amazonaws.com/EeveeStageProd/eeveeget
-curl -X POST -H "Content-Type: application/json" -d '{"number":20}' https://vgwwoe4uhc.execute-api.ap-northeast-1.amazonaws.com/Prod/yadonget
+curl -X POST -H "Content-Type: application/json" -d '{"number":20}' https://rafpp1kc39.execute-api.ap-northeast-1.amazonaws.com/Stage/yadonget
 echo
